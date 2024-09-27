7decd35c91bc5b68a76a6033fdd8073b1c7a8915
Corvin Szimion <corvin.szimion@whw.de>
2024-09-27 15:26:59 +0200

    add only .NET 8 benchmarks

--- JsonBenchmarks/Benchmarks.cs
+++ JsonBenchmarks/Benchmarks.cs
@@ -5,35 +5,34 @@ using Newtonsoft.Json;
 namespace JsonBenchmarks;
 
 [MemoryDiagnoser]
-[GroupBenchmarksBy(BenchmarkDotNet.Configs.BenchmarkLogicalGroupRule.ByCategory)]
-[CategoriesColumn]
+[SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net80)]
 public class Benchmarks
 {
-	private List<Person> _people = new List<Person>();
+    private List<Person> _people = new List<Person>();
 
-	[GlobalSetup]
-	public void Setup()
-	{
-		// Create a Faker instance with the desired locale
-		var faker = new Faker<Person>();
+    [GlobalSetup]
+    public void Setup()
+    {
+        // Create a Faker instance with the desired locale
+        var faker = new Faker<Person>();
 
-		// Generate a list of fake persons
-		_people = faker
-			.RuleFor(p => p.ID, f => f.IndexFaker)
-			.RuleFor(p => p.FirstName, f => f.Person.FirstName)
-			.RuleFor(p => p.LastName, f => f.Person.LastName)
-			.Generate(500); // You can change this number to generate more or fewer persons
-	}
+        // Generate a list of fake persons
+        _people = faker
+            .RuleFor(p => p.ID, f => f.IndexFaker)
+            .RuleFor(p => p.FirstName, f => f.Person.FirstName)
+            .RuleFor(p => p.LastName, f => f.Person.LastName)
+            .Generate(500); // You can change this number to generate more or fewer persons
+    }
 
-	[Benchmark]
-	public string NewtonSoft_Serialization()
-	{
-		return JsonConvert.SerializeObject(_people);
-	}
+    [Benchmark]
+    public string NewtonSoft_Serialization()
+    {
+        return JsonConvert.SerializeObject(_people);
+    }
 
-	[Benchmark]
-	public string BCL_Serialization()
-	{
-		return System.Text.Json.JsonSerializer.Serialize(_people);
-	}
+    [Benchmark]
+    public string BCL_Serialization()
+    {
+        return System.Text.Json.JsonSerializer.Serialize(_people);
+    }
 }
